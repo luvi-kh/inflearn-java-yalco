1a696be92a6b3486651ff0b71539fecc21cb4bac
luvi-kh <nalsengom@daum.net>
2024-11-15 21:28:16 +0900

    sec05/ch10 싱글톤
    Singleton

---
+++ src/sec05/ch10/Tab.java
@@ -0,0 +1,10 @@
+package sec05.ch10;
+
+public class Tab {
+    //  ⭐️ 공유되는 유일한 인스턴스를 받아옴
+    private Setting setting = Setting.getInstance();
+
+    public Setting getSetting() {
+        return setting;
+    }
+}
