6dfa48abc434761221c99165e7badb0450d0e94e
luvi-kh <nalsengom@daum.net>
2024-11-17 13:48:48 +0900

    sec06/ch07
    레코드

---
+++ src/sec06/ch07/ex01/Child.java
@@ -0,0 +1,8 @@
+package sec06.ch07.ex01;
+
+// ⭐️  레코드로 작성
+public record Child(
+        String name,
+        int birthYear,
+        Gender gender
+) {}
---
+++ src/sec06/ch07/ex02/InfoPrinter.java
@@ -0,0 +1,6 @@
+package sec06.ch07.ex02;
+
+public interface InfoPrinter {
+    void printInfo();
+}
+
