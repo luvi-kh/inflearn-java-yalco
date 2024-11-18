08cd80c8312df59c9f58aa172722b3545eaf9396
luvi-kh <nalsengom@daum.net>
2024-11-18 21:49:21 +0900

    sec09/ch01  FunctionalInterface

---
+++ src/sec09/ch01/DoubleParam.java
@@ -0,0 +1,6 @@
+package sec09.ch01;
+
+@FunctionalInterface
+public interface DoubleParam {
+    int func(int a, int b);
+}
---
+++ src/sec09/ch01/SingleParam.java
@@ -0,0 +1,6 @@
+package sec09.ch01;
+
+@FunctionalInterface
+public interface SingleParam {
+    int func (int i);
+}
