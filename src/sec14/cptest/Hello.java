4af07322e640194c02cf9828b58665acb18fce98
luvi-kh <nalsengom@daum.net>
2024-11-21 20:43:56 +0900

    sec14/cptest  소스패스와 클래스패스

---
+++ src/sec14/cptest/Hello.java
@@ -0,0 +1,7 @@
+package sec14.cptest;
+
+public class Hello {
+    public String sayHello () {
+        return "Hello!";
+    }
+}
---
+++ src/sec14/cptest/Main.java
@@ -0,0 +1,7 @@
+package sec14.cptest;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Hello().sayHello());
+    }
+}
