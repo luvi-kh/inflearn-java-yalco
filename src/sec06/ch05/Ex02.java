6ca355d7aab8e65a9100c03729fb6eb53f2a8768
luvi-kh <nalsengom@daum.net>
2024-11-22 01:46:14 +0900

    sec06/ch05  메인메소드

---
+++ src/sec06/ch05/Ex02.java
@@ -0,0 +1,10 @@
+package sec06.ch05;
+
+public class Ex02 {
+    public static void main(String[] args) {
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
