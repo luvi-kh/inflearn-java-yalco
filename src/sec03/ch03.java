efcf3305166a2c0326ce012a504827bc6d15764b
luvi-kh <nalsengom@daum.net>
2024-11-23 17:37:27 +0900

    sec03/ch03  문자 자료형

--- src/sec03/ch03.java
+++
@@ -1,9 +0,0 @@
-package sec03;
-
-public class ch03 {
-    public static void main(String[] args) {
-        //  💡 아래의 기능으로 문자가 의미하는 정수로 변환
-        int int_d1 = Character.getNumericValue('1');
-        int int_d2 = Character.getNumericValue('2');
-    }
-}
