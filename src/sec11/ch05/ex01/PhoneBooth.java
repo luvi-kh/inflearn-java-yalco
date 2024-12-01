3bbae9f15fb7924507039f1933964e62e9511c46
luvi-kh <nalsengom@daum.net>
2024-12-02 01:52:31 +0900

    sec11

--- src/sec11/ch05/ex01/PhoneBooth.java
+++ src/sec11/ch05/ex01/PhoneBooth.java
@@ -11,13 +11,13 @@ public class PhoneBooth {
 
         //  💡 아래를 해제하지 않으면 첫 사용자가 혼자 다 씀
         //  ℹ️ 단, JDK 제품마다 차이가 있을 수 있습니다.
-        //notifyAll();
-        //try {
+        notifyAll();
+        try {
         //    //  💡 현 사용자를 폰부스에서 내보냄
         //    //  - sleep처럼 아래의 예외 반환 확인
-        //    wait();
-        //} catch (InterruptedException e) {
-        //    throw new RuntimeException(e);
-        //}
+            wait();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
